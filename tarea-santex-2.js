2187842d96b6fe1c88d7c9c40df60b9f4285ab98
kevin joel noviello <kjnoviello@gmail.com>
2023-05-10 19:50:36 -0300

    tarea2.js

--- tarea-santex-2.js
+++ tarea-santex-2.js
@@ -153,9 +153,9 @@ class Producto {
             this.stock = stock;
         } else {
             this.stock = 10;
-        }
-    }
-}
+        };
+    };
+};
 
 // Creo todos los productos que vende mi super
 const queso = new Producto('KS944RUR', 'Queso', 10, 'lacteos', 4);
@@ -169,7 +169,6 @@ const jabon = new Producto('WE328NJ', 'Jabon', 4, 'higiene', 3);
 
 const productosDelSuper = [queso, gaseosa, cerveza, arroz, fideos, lavandina, shampoo, jabon];
 
-
 class Carrito {
     productos;      // Lista de productos agregados
     categorias;     // Lista de las diferentes categorías de los productos en el carrito
@@ -182,68 +181,85 @@ class Carrito {
     }
     async agregarProducto(sku, cantidad) {
         console.log(`Agregando ${cantidad} ${sku}`);
-        
-        // Busco el producto en la "base de datos"
-        const producto = await findProductBySku(sku);
-        
-        // me fijo si el producto ya existe
-        const existeProducto = this.productos.find(item => item.sku === sku);
-        
-        // si existe solo le sumo la cantidad
-        if (existeProducto) {
-            existeProducto.cantidad += cantidad;
 
-        // si no existe lo agrego y tambien la categoria.
-        } else {
-            const nuevoProducto = new ProductoEnCarrito(sku, producto.nombre, cantidad);
-            this.productos.push(nuevoProducto);
-            this.categorias.push(producto.categoria);
-        // if (!this.categorias.includes(producto.categoria)) {
-        //     this.categorias.push(producto.categoria);}
+
+        try {
+            // Busco el producto en la "base de datos"
+            const producto = await findProductBySku(sku);
+            
+            // me fijo si el producto ya existe
+            const existeProducto = this.productos.find(item => item.sku === sku);
+            
+            // si existe solo le sumo la cantidad
+            if (existeProducto) {
+                existeProducto.cantidad += cantidad;
+
+            // si no existe lo agrego y tambien verifico si existe la misma categoria a agregar.
+            } else {
+                const nuevoProducto = new ProductoEnCarrito(sku, producto.nombre, cantidad);
+                this.productos.push(nuevoProducto);
+                // this.categorias.push(producto.categoria);
+                if (!this.categorias.includes(producto.categoria)) {
+                    this.categorias.push(producto.categoria);}
+            };
+
+            // se modifica el precio
+            this.precioTotal = this.precioTotal + (producto.precio * cantidad);
+
+        // manejo del error al no econtrar un producto
+        } catch (err) {
+            console.log(`Errorrrrrrr al agregar,`, err);
         };
-
-        // se modifica el precio
-        this.precioTotal = this.precioTotal + (producto.precio * cantidad);
     };
+
 
     // funcion eliminar producto
     async eliminarProducto(sku, cantidad) {
         console.log(`Eliminando ${cantidad} ${sku}`);
 
-        // busco el producto en la "base de datos"
-        const producto = await findProductBySku(sku);
-        console.log("el producto encontrado a modificar es: ", producto);
-        
-
-        // me fijo si el producto ya existe-------------------------------------------
-        const existeProducto = this.productos.find(item => item.sku === sku);
-        console.log("cantidad del producto en el carrito: ", existeProducto.cantidad);
-        console.log("cantidad del producto a eliminar: ", cantidad);
-        
-        // si la cantidad a eliminar es menor que lo que hay en el producto se modifica la cantidad
-        if (existeProducto && existeProducto.cantidad > cantidad) {
-            existeProducto.cantidad -= cantidad;
-            console.log("cantidad del producto luego de modificarlo: ", existeProducto.cantidad);
+        try {
+            // busco el producto en la "base de datos"
+            const producto = await findProductBySku(sku);
+            console.log("el producto encontrado a modificar es: ", producto);
             
-        // si la cantidad a eliminar es mayor o igual que lo que hay en el producto se elimina el producto y la categoria
-        } else if (existeProducto && existeProducto.cantidad <= cantidad){
-            const positionExisteProducto = this.productos.indexOf(existeProducto);
-            this.productos.splice(positionExisteProducto, 1);
+    
+            // me fijo si el producto ya existe
+            const existeProducto = this.productos.find(item => item.sku === sku);
+            console.log("cantidad del producto en el carrito: ", existeProducto.cantidad);
+            console.log("cantidad del producto a eliminar: ", cantidad);
+            
+            // si la cantidad a eliminar es menor que lo que hay en el producto se modifica la cantidad
+            if (existeProducto && existeProducto.cantidad > cantidad) {
+                // const positionExisteProducto = this.productos.indexOf(existeProducto);
+                // this.productos.splice(positionExisteProducto, 1);
+                existeProducto.cantidad -= cantidad;
+                console.log("cantidad del producto luego de modificarlo: ", existeProducto.cantidad);
+                
+            // si la cantidad a eliminar es mayor o igual que lo que hay en el producto se elimina el producto y la categoria
+            } else if (existeProducto && existeProducto.cantidad <= cantidad){
+                const positionExisteProducto = this.productos.indexOf(existeProducto);
+                this.productos.splice(positionExisteProducto, 1);
+    
+                console.log("categorias antes de eliminar el producto: ", this.categorias);
+                const positionExisteCategoria = this.categorias.indexOf(producto.categoria);
+                this.categorias.splice(positionExisteCategoria, 1);
+                console.log("categorias luego de eliminar todas las unidades del producto: ", this.categorias);
+    
+    
+            // si no hay producto en el carrito
+            } else {
+                console.log("no hay tal producto");
+            };
 
-            console.log("categorias antes de eliminar el producto: ", this.categorias);
-            const positionExisteCategoria = this.categorias.indexOf(producto.categoria);
-            this.categorias.splice(positionExisteCategoria, 1);
-            console.log("categorias luego de eliminar todas las unidades del producto: ", this.categorias);
+            // se modifica el precio
+            this.precioTotal = this.precioTotal - (producto.precio * cantidad);
+            console.log("El carrito despues de eliminar el producto queda conformado por: ", carrito.productos,);
+            console.log("el precio total en el carrito es de: ", carrito.precioTotal);
 
-        // si no hay producto en el carrito
-        } else {
-            console.log("no hay tal producto");
+        // manejo del error al no econtrar un producto
+        } catch (err) {
+            console.error("Errorrrrrr, no se puede eliminar un producto que no fue agregado,", err);
         };
-
-        // se modifica el precio
-        this.precioTotal = this.precioTotal - (producto.precio * cantidad);
-        console.log("El carrito despues de eliminar el producto queda conformado por: ", carrito.productos,);
-        console.log("el precio total en el carrito es de: ", carrito.precioTotal);
     };
 };
 
@@ -267,22 +283,23 @@ function findProductBySku(sku) {
             if (foundProduct) {
                 resolve(foundProduct);
             } else {
-                reject(`Product ${sku} not found`);
+                reject(`No existe el producto ${sku}`);
             }
         }, 1500);
     });
-}
+};
 
 const carrito = new Carrito();
 console.log("el carrito inicial esta conformado por: ", carrito);
-carrito.agregarProducto('KS944RUR', 2);
-carrito.agregarProducto('XX92LKI', 5);
-carrito.agregarProducto('WE328NJ', 1);
-carrito.agregarProducto('XX92LKI', 3);
+carrito.agregarProducto('KS944RUR', 2);     // agrego 2 unidades de queso
+carrito.agregarProducto('XX92LKI', 5);      // agrego 5 unidades de arroz
+carrito.agregarProducto('KS944RUR', 1);     // agrego 1 unidad de queso, de nuevo para verificar que no duplique la categoria
+carrito.agregarProducto('AA92LKI', 8);      // agrego 8 unidades de un producto que no existe
+
 
 // al eliminar un producto lo puse dentro de un timeout para simular que primero se agrega y despues se elimina
 setTimeout(() => {
     console.log("el carrito antes de modificarse esta conformado por: ", carrito);
-    carrito.eliminarProducto('XX92LKI', 2);
+    carrito.eliminarProducto('XX92LKI', 2);      // elimino 2 unidades de arroz previamente agregados
+    carrito.eliminarProducto('MM763KK', 1);      // elimino 1 unidades de un producto que no existe
 }, 2000);
-
